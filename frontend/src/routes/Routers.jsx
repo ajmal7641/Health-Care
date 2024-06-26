d1b3e7f7d217e79962bdbf9a77a422af16836a3e
Ajmal <ajmalks7641@gmail.com>
2024-06-26 09:15:03 +0530

    health-care project

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,34 @@
+
+
+
+
+import Home from '../pages/Home'
+import Contact from '../pages/Contact'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorsDetails from '../pages/Doctors/DoctorsDetails'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Service from '../pages/Service'
+
+import {Routes , Route} from 'react-router-dom'
+
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/doctors" element={<Doctors/>}/>
+      <Route path="/doctors/:id" element={<DoctorsDetails/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Signup/>}/>
+      <Route path="/service" element={<Service/>}/>
+
+
+      
+    </Routes>
+  )
+}
+
+export default Routers

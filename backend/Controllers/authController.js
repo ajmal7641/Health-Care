d636e483c4a700b3ae52e89d9f1dc1f6b8b29b63
Ajmal <ajmalks7641@gmail.com>
2024-07-05 09:07:31 +0530

    health-care project backend started

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,70 @@
+import User from "../models/UserSchema"
+import Doctor from '../models/DoctorSchema'
+import jwt from "jsonwebtoken"
+import bcrypt from 'bcryptjs'
+
+export const register = async (req, res ) => { 
+
+      const { email, password, name, role, photo, gender } = req.body
+
+
+      try {
+
+            let user =null
+
+            if(role==='patient'){
+                  user = User.findOne({email})
+            }
+            else if(role==='doctor'){
+                  user = Doctor.findOne({email})
+
+            }
+
+            // check if user exist
+            if(user){
+                  return res.status(400).json({message:'user already exist'})
+            }
+
+            // hash password 
+            const salt = await bcrypt.genSalt(10)
+            const hashPassword = await bcrypt.hash(password,salt)
+
+            if(role === 'patient'){
+                  user = new User({
+                        name,
+                        email,
+                        password : hashPassword,
+                        photo,
+                        gender,
+                        role
+                  })
+            }
+
+            if(role === 'doctor'){
+                  user = new Doctor({
+                        name,
+                        email,
+                        password : hashPassword,
+                        photo,
+                        gender,
+                        role
+                  })
+            }
+
+            await user.save()
+
+            res.status(200).json({success:true , message:'User Succesfully created'})
+            
+      } catch (err){    
+
+            res.status(500).json({success:false, message:'internal server error, Try again'})
+      }
+}
+
+
+export const login = async (req, res ) => {
+      try {
+            
+      } catch (err){    
+      }
+}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import mongoose from "mongoose"
 import { config } from "dotenv"
+import authRoute from './Routes/auth.js'
 
 
 config()
@@ -18,6 +19,25 @@ app.get('/', (req,res) => {
       res.send('Api is working ')
 })
 
+mongoose.set('strictQuery',false)
+const connectDB = async() => {
+      try {
+           await  mongoose.connect(process.env.MONGO_URL)
+            console.log("MongoDB database is connected");
+      } catch (err) {
+            console.log('Mongo databse connection is failed');
+      }
+}
+
+// middleware
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+app.use('/api/v1/auth',authRoute) //domain/api/v1/auth/register
+
+
 app.listen(port, ()=>{
+      connectDB()
       console.log('Server  is Running on port ' +port);
 })
